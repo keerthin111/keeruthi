0d19f359a30f8a2d7d4cc94067879a805f793247
keerthin111 <97940146+keerthin111@users.noreply.github.com>
2022-02-25 12:13:05 +0530

    Add files via upload

---
+++ program7.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace ConsoleApp10
+{
+    class RegisterNum
+    {
+        int regNo;
+        static int startNum;
+        static RegisterNum()
+        {
+            startNum = 20210000;
+        }
+        RegisterNum()
+        {
+            regNo = ++startNum;
+
+        }
+        public static void Main(string[]args)
+        {
+            for(int i=0;i<100;i++)
+            {
+                RegisterNum student = new RegisterNum();
+                Console.WriteLine("student{0} : {1}", i + 1, student.regNo);
+            }
+        }
+    }
+}
