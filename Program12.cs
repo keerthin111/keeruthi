a0f2f65007a821bb35ea8a8d0b021d3b1bd5081a
keerthin111 <97940146+keerthin111@users.noreply.github.com>
2022-02-25 12:16:08 +0530

    Add files via upload

---
+++ Program12.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.IO;
+namespace ConsoleApp18
+{
+    class FileRead
+    {
+        public static void Main()
+        {
+            string file1;
+            string file2;
+            Console.Write("enter the first file path:");
+            file1 = Console.ReadLine();
+            Console.Write("enter the second file path:");
+            file2 = Console.ReadLine();
+            if (!File.Exists(file1))
+            {
+                Console.WriteLine("first file does not exists");
+            }
+            else
+                if (!File.Exists(file2))
+            {
+                Console.WriteLine("second file does not exist!");
+            }
+            else
+               if (File.ReadAllText(file1) == File.ReadAllText(file2))
+            {
+                Console.WriteLine("both files contain the same content");
+            }
+
+            else
+            {
+                Console.WriteLine("contents of files are not same");    
+            }
+        }
+    }
+} 
