80dc9deb8ec193f7ae339311ebada10205945578
keerthin111 <97940146+keerthin111@users.noreply.github.com>
2022-02-25 12:11:49 +0530

    Add files via upload

---
+++ Program8.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace ConsoleApp11
+{
+    class FrequencyIS
+    {
+     static void Main(string[]args)
+        {
+            int count = 0; 
+            string inputString;
+            Console.WriteLine("\n-------------Frequency of word 'is'------------");
+            Console.Write("\nenter the input string:");
+            inputString = Console.ReadLine();
+            char[] separator = { ',',' ', '.','!', '\n' };
+            string testString = inputString.ToLower();
+            string[] outcomes = testString.Split(separator);
+            foreach(String s in outcomes)
+            {
+                Console.WriteLine(s);
+                if (s == "is") 
+                count++;
+
+            }
+            Console.WriteLine("\n Number of 'is' in" + inputString + "is:" + count);
+
+        }
+    }
+}
